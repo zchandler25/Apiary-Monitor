5f1dc388a1efecbc3c666329a7642242fb91d0be
Zachary <73605356+zchandler25@users.noreply.github.com>
2023-03-25 15:42:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,59 @@
-from machine import Pin
-from time import sleep
-import dht
- 
-sensor = dht.DHT22(Pin(2)) 
- 
+from machine import Pin, I2C
+from ssd1306 import SSD1306_I2C
+import utime as time
+from dht import DHT22
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+# InfluxDB Cloud account details
+bucket = "my-bucket"
+org = "my-org"
+token = "my-token"
+url = "https://us-west-2-1.aws.cloud2.influxdata.com"
+
+# Initialize InfluxDB client
+client = InfluxDBClient(url=url, token=token)
+
+# Display
+i2c = I2C(0, scl=Pin(1), sda=Pin(0), freq=200000)
+oled = SSD1306_I2C(WIDTH, HEIGHT, i2c)
+WIDTH = 128
+HEIGHT = 32
+
+# Sensor
+sensor = DHT22(Pin(6))
+
+# LED
+led = machine.Pin('LED', machine.Pin.OUT)
+
+# Main loop
 while True:
+    led.on()
+    time.sleep(0.5)
+    led.off()
+    time.sleep(0.5)
+    
     sensor.measure()
     temp = sensor.temperature()
     hum = sensor.humidity()
+    weight = ("150")
     print("Temperature: {}°C   Humidity: {:.0f}% ".format(temp, hum))
-    sleep(2)
+
+    # Write sensor data to InfluxDB Cloud
+    write_api = client.write_api(write_options=SYNCHRONOUS)
+    point = Point("my-measurement").tag("location", "my-location").field("temperature", temp).field("humidity", hum)
+    write_api.write(bucket=bucket, org=org, record=point)
+    
+    # Clear the OLED display and add text
+    oled.fill(0)       
+    oled.text("Temp: ",0,0)
+    oled.text(str(temp),50,0)
+    oled.text("C",90,0)
+    oled.text("Hum: ",0,10)
+    oled.text(str(hum),50,10)
+    oled.text("%",90,10)
+    oled.text("LB: ",0,20)
+    oled.text(str(weight),50,20)
+    
+    time.sleep(1)
+    oled.show()
