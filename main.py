405372abf178f160f2cf611a8b8d66aff4cd0a45
Zachary <73605356+zchandler25@users.noreply.github.com>
2023-04-13 14:13:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,80 @@
-from machine import Pin, I2C
-from ssd1306 import SSD1306_I2C
-import utime as time
+import socket
+import time
+from machine import Pin
 from dht import DHT22
-time.sleep(1.5)
+import network
+import secrets
 
-###################################################################################
-#Display
-i2c = I2C(0, scl=Pin(1), sda=Pin(0), freq=200000)       # Init I2C using pins GP8 & GP9 (default I2C0 pins) 
-oled = SSD1306_I2C(WIDTH, HEIGHT, i2c)                  # Init oled display
-WIDTH  = 128                                            # oled display width
-HEIGHT = 32                                             # oled display height
- ###################################################################################
-sensor = DHT22(Pin(6))
-led= machine.Pin('LED', machine.Pin.OUT)
+# WiFi network details
+SSID = secrets.secrets['ssid']
+PASSWORD = secrets.secrets['password']
 
-#Main loop
+# InfluxDB Cloud settings
+INFLUXDB_URL = 'https://us-east-1-1.aws.cloud2.influxdata.com'
+INFLUXDB_TOKEN = 'aBKAtdFnwGkKWlo9bNuuUfBVX2IQcMeogVh1buDryed9z6vbPcU_MdKQ2k4ZuGxGBR03JV5e7rEV14QDxXjSlw=='
+INFLUXDB_ORG = '728e39e951451c80'
+INFLUXDB_BUCKET = 'e8f3f6b06d47534f'
+
+# Connect to WiFi
+wifi = network.WLAN(network.STA_IF)
+wifi.active(True)
+wifi.connect(SSID, PASSWORD)
+
+# Wait until connected
+while not wifi.isconnected():
+    pass
+
+# Connect to the DHT22 sensor
+dht22 = DHT22(Pin(6))
+
+# Initialize dictionaries to store the last 5 readings of temperature and humidity
+temperature_dict = {}
+humidity_dict = {}
+
 while True:
-    led.on()
-    time.sleep(0.5)
-    led.off()
-    time.sleep(0.5)
-    
-    sensor.measure()
-    temp = sensor.temperature()
-    hum = sensor.humidity()
-    weight = ("150")
-    print("Temperature: {}°C   Humidity: {:.0f}% ".format(temp, hum))
-    # Clear the oled display in case it has junk on it.
-    oled.fill(0)       
-    
-    # Add some text
-    oled.text("Temp: ",0,0)
-    oled.text(str(temp),50,0)
-    oled.text("C",90,0)
-    
-    oled.text("Hum: ",0,10)
-    oled.text(str(hum),50,10)
-    oled.text("%",90,10)
-    
-    oled.text("LB: ",0,20)
-    oled.text(str(weight),50,20)
-    
-    time.sleep(1)
-    oled.show()
+    try:
+        # Read the temperature and humidity from the DHT22 sensor
+        dht22.measure()
+        temperature = dht22.temperature()
+        humidity = dht22.humidity()
+
+        # Get the current time in Unix timestamp format
+        timestamp = int(time.time())
+
+        # Construct the data payload in InfluxDB Line Protocol format with timestamp
+        data = 'temperature,location=office value={} {}'.format(temperature, timestamp) + '\n'
+        data += 'humidity,location=office value={} {}'.format(humidity, timestamp) + '\n'
+
+        # Print the data payload for debugging purposes
+        print('Data payload: {}'.format(data))
+
+        # Send the data to InfluxDB Cloud using the POST method and sockets
+        headers = {
+            'Authorization': 'Token {}'.format(INFLUXDB_TOKEN),
+            'Content-Type': 'application/octet-stream',
+            'User-Agent': 'micropython-sockets'
+        }
+        url = '/api/v2/write?org={}&bucket={}&precision=s'.format(INFLUXDB_ORG, INFLUXDB_BUCKET)
+        message = 'POST {} HTTP/1.1\r\nHost: {}\r\n{}\r\n{}'.format(url, INFLUXDB_URL[8:], headers, data)
+        s = socket.socket()
+        s.connect(socket.getaddrinfo(INFLUXDB_URL[8:], 443)[0][-1])
+        s = ssl.wrap_socket(s)
+        s.write(message)
+        response = s.read(1024)
+        s.close()
+
+        # Print the response code and text for debugging purposes
+        print('Response code: {}'.format(response.decode().split()[1]))
+        print('Response text: {}'.format(response.decode().split('\r\n\r\n')[1]))
+
+        # Store the latest reading of temperature and humidity in their respective dictionaries
+        temperature_dict[timestamp] = temperature
+        humidity_dict[timestamp] = humidity
+
+        # Remove the oldest reading from the dictionaries if there are more than 5 readings
+        if len(temperature_dict) > 5:
+            oldest_timestamp = min(temperature_dict.keys())
+            del temperature_dict[oldest_timestamp]
+        if len(humidity_dict) > 5:
+            oldest_timestamp = min(humidity_dict.keys())
+           
